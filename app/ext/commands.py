2f71aa574213c4d760e4db4b441dc9f2ce3bd629
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2022-11-13 10:07:46 -0300

    add faker

--- app/ext/commands.py
+++ app/ext/commands.py
@@ -1,5 +1,7 @@
 from app.ext.database import db
 from app.models import Employee, Company
+from faker import Faker
+from random import choice
 
 
 def create_db():
@@ -14,6 +16,8 @@ def drop_db():
 
 def populate_db():
     """Populate db with sample data"""
+    fake = Faker()
+    
     company_data = [
         Company(name='UFC'),
         Company(name='UECE'),
@@ -22,34 +26,14 @@ def populate_db():
     employeer_data = [
         Employee(
             id=i, 
-            name='Rodrigo', 
-            war_name='Rodrigo Meneses', 
-            role='Analista',
-            identification='2000510231209',
-            admission='26/12/2021',
-            company_id=1, 
-            to_print=True
+            name=fake.name(), 
+            # war_name='Rodrigo Meneses', 
+            # role='Analista',
+            # identification='2000510231209',
+            # admission='26/12/2021',
+            company_id=choice([1, 2]), 
+            to_print=choice([True, False])
         ) for i in range(25)
-        # Employee(
-        #     id='2', 
-        #     name='Marta',
-        #     war_name='Marta Saraiva', 
-        #     role='Processamento de dados',
-        #     identification='2000545321094',
-        #     admission='26/11/2021',
-        #     company_id=1, 
-        #     to_print=True
-        # ),
-        # Employee(
-        #     id='3', 
-        #     name='Liz',
-        #     war_name='Maria Liz', 
-        #     role='Contabilista',
-        #     identification='7845645321094',
-        #     admission='26/11/2010',
-        #     company_id=2, 
-        #     to_print=False
-        # )
     ]
     db.session.bulk_save_objects(company_data)
     db.session.bulk_save_objects(employeer_data)
